cb35e5bcdb227530ff93f8f41953e01848e1f464
Cristina <ctff000@hotmail.com>
2024-04-04 19:19:41 +0200

    Organizamos Scrit principal

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ import deterministic as det
 import multi_scenario as ms
 import machine_learning as ml
 
+####################################################################################################
+# AUXILIAR FUNCTIONS     ###########################################################################
+####################################################################################################
+
 def select_execution_data(demandData, nodeData, max_num_nodes, date):
     random.seed(10051347)
     codnode_list = list(demandData[demandData['Date'] == date]['codnode'].values)
@@ -19,10 +23,15 @@ def select_execution_data(demandData, nodeData, max_num_nodes, date):
     if number_nodes > max_num_nodes:
         codnode_list = random.sample(codnode_list, max_num_nodes)
     nodeDataModel = nodeData[nodeData['codnode'].isin(codnode_list)]
-    demandDataModel = demandData[(demandData['Date'] < date) & (demandData['codnode'].isin(codnode_list))]
-    realDemand= demandData[(demandData['Date'] == date) & (demandData['codnode'].isin(codnode_list))]
+    demandDataModel = demandData[(demandData['Date'] < date) & 
+                                 (demandData['codnode'].isin(codnode_list))]
+    realDemand= demandData[(demandData['Date'] == date) & (demandData['codnode']
+                                                           .isin(codnode_list))]
     return nodeDataModel, demandDataModel, realDemand
 
+####################################################################################################
+# AUXILIAR VARIABLES     ###########################################################################
+####################################################################################################
 month_dict = {
     1: 'January',
     2: 'February',
@@ -39,6 +48,10 @@ month_dict = {
 }
 month_dict_reversed = {value: key for key, value in month_dict.items()}
 
+####################################################################################################
+# STREAMLIT CONFIGURATION    #######################################################################
+####################################################################################################
+
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # @st.cache_data #Takes you cache_resource arguments for machine learning 
@@ -48,9 +61,14 @@ st.title('Traveling Salesman Problem under Demand Uncertainty :truck:')
 st.markdown('This application uses several methods to solve the stochastic problem.')
 st.divider()
 
-# SIDE BAR
+####################################################################################################
+# SIDE BAR CONFIGURATION ###########################################################################
+####################################################################################################
 st.sidebar.header('Configuration', divider='red')
 
+###################################################################################################
+# FILES                  ##########################################################################
+###################################################################################################
 # Select file from computer
 nodeData = st.sidebar.file_uploader(":open_file_folder: Node File", type=["csv"])
 if nodeData is not None:
@@ -62,30 +80,49 @@ if demandData is not None:
     demandData['Date'] = pd.to_datetime(demandData['Date'])
     st.session_state["demandData"] = demandData
 
+###################################################################################################
+# SELECT DATE            ##########################################################################
+###################################################################################################
 st.sidebar.subheader('Day we want to solve', divider='red')
 if demandData is not None:
     year = st.sidebar.selectbox('Select the year', options=[2023, 2024])
-    months = [month_dict[i] for i in list(demandData[demandData['Year'] == year]['Month'].sort_values())]
+    months = [month_dict[i] for i in list(demandData[demandData['Year'] == year]
+                                          ['Month'].sort_values())]
     month_name = st.sidebar.select_slider('Select the month', options=months)
     month = month_dict_reversed[month_name]
-    days = list(demandData[(demandData['Year'] == year) & (demandData['Month'] == month)]['Day'].sort_values())
+    days = list(demandData[(demandData['Year'] == year) & (demandData['Month'] == month)]
+                ['Day'].sort_values())
     day = st.sidebar.select_slider('Select the day', options=days)
-
     date = datetime(year, month, day)
 
+###################################################################################################
+# SELECT METHOD          ##########################################################################
+###################################################################################################
 st.sidebar.subheader('Method', divider='red')
 # Drop-down controls and bars
-method = st.sidebar.selectbox('Select Method', ['Deterministic', 'Multi-scenario', 'Machine Learning'])
+method = st.sidebar.selectbox('Select Method', ['Deterministic', 'Multi-scenario', 
+                                                'Machine Learning'])
 
+###################################################################################################
+# SELECT PARAMETERS     ###########################################################################
+###################################################################################################
 # Others parameters for the methods
 st.sidebar.subheader('Parameters', divider='red')
 if method == 'Multi-scenario':
     num_scenarios = st.sidebar.number_input('Number of Scenarios', min_value=1, step=1, value=30)
-    ms_option = st.sidebar.selectbox('Options', ['Maximum expectation', 'Conditional Value at Risk (CVaR)', 'Worst Case Analysis'])
+    ms_option = st.sidebar.selectbox('Options', ['Maximum expectation', 
+                                                 'Conditional Value at Risk (CVaR)', 
+                                                 'Worst Case Analysis'])
 elif method == 'Machine Learning':
-    ml_option = st.sidebar.selectbox('Machine Learning Options', ['RNNs', 'CNNs', 'Attention Models', 'DNN', 'Ensemble Models', 'Gaussian Processes', 'Hidden Markov Models'])
+    ml_option = st.sidebar.selectbox('Machine Learning Options', ['RNNs', 'CNNs', 'Attention Models', 
+                                                                  'DNN', 'Ensemble Models', 
+                                                                  'Gaussian Processes', 
+                                                                  'Hidden Markov Models'])
 max_num_nodes = st.sidebar.slider('Max number of points', min_value=3, max_value=40, value=15)
 
+####################################################################################################
+# EXECUTE METHODS        ###########################################################################
+####################################################################################################
 # Optional display of nodes
 if st.sidebar.button('Solve', type='primary', use_container_width=True ):
     if "nodeData" not in st.session_state or "demandData" not in st.session_state:
@@ -95,30 +132,40 @@ if st.sidebar.button('Solve', type='primary', use_container_width=True ):
             st.warning('We need a demand data file', icon="⚠️")
         st.stop()
 
-    nodeDataSelected, demandDataSelected, realDemand = select_execution_data(demandData, nodeData, max_num_nodes, date)
+    nodeDataSelected, demandDataSelected, realDemand = select_execution_data(demandData, 
+                                                                             nodeData, 
+                                                                             max_num_nodes, 
+                                                                             date)
 
     # Mostrar el spinner
     if method == 'Deterministic':
         with st.spinner('Executing model'):
-            result = det.execute(nodeData=nodeDataSelected, realDemand=realDemand, demandData=demandDataSelected) 
+            result = det.execute(nodeData=nodeDataSelected, realDemand=realDemand, 
+                                 demandData=demandDataSelected) 
         st.empty()
         st.session_state["result"] = result
     elif method == 'Multi-scenario':
         with st.spinner('Executing model'):
-            result = ms.execute(num_scenarios=num_scenarios, option=ms_option, nodeData=nodeDataSelected, demandData=demandDataSelected, realDemand=realDemand) 
+            result = ms.execute(num_scenarios=num_scenarios, option=ms_option, 
+                                nodeData=nodeDataSelected, demandData=demandDataSelected, 
+                                realDemand=realDemand) 
         st.empty()
         st.session_state["result"] = result
     elif method == 'Machine Learning':
         st.warning('We are working on it', icon="🔧")
         st.stop()
         with st.spinner('Executing model'):
-            result = ml.execute(num_nodos=num_nodes, option=ml_option, nodeData=nodeData, demandData=demandData) 
+            result = ml.execute(num_nodos=num_nodes, option=ml_option, nodeData=nodeData, 
+                                demandData=demandData) 
         st.empty()
         st.session_state["result"] = result
     else:
         st.warning('We are working on it', icon="🔧")
         st.stop()
 
+####################################################################################################
+# PRINT RESULTS          ###########################################################################
+####################################################################################################
 if "result" in st.session_state:
     result = st.session_state["result"]
     nodeDataSelected = result['nodeDataSelected']
@@ -126,12 +173,21 @@ if "result" in st.session_state:
     
     st.header('Result:', divider='red')
 
+    ###################################################################################################
+    # OBJECTIVE FUNCTION    ###########################################################################
+    ###################################################################################################
     # Show Objective Function
     st.subheader('**Objective Function:**')
     st.markdown(f'***Method Objective function:*** {round(result['optimum_value'],2)}')
-    st.markdown(f'***Distance travelled + Undelivered demand:*** {round(result['total_distance'] + np.sum(result['nodes_demand']) - result['capacity_used'] ,2)}')
-
-    m = folium.Map(location=[nodeDataSelected['latitude'].values.mean(), nodeDataSelected['longitude'].values.mean()], zoom_start=11, tiles = "CartoDB Positron")
+    st.markdown(f'***Distance travelled + Undelivered demand:*** {round(result['total_distance'] 
+                                                                        + np.sum(result['nodes_demand']) 
+                                                                        - result['capacity_used'] ,2)}')
+    ###################################################################################################
+    # MAP                   ###########################################################################
+    ###################################################################################################
+    m = folium.Map(location=[nodeDataSelected['latitude'].values.mean(), 
+                             nodeDataSelected['longitude'].values.mean()], zoom_start=11, 
+                             tiles = "CartoDB Positron")
     for i in range(result['num_nodes']):
         popup_html = """
         <div style="width: 200px;">
@@ -166,12 +222,17 @@ if "result" in st.session_state:
                 st.session_state['coordinates'] = coordinates
             else: 
                 coordinates = st.session_state['coordinates']
-            folium.plugins.AntPath(locations=coordinates, dash_array=[8, 100], delay=800, color='red').add_to(m)
+            folium.plugins.AntPath(locations=coordinates, dash_array=[8, 100], delay=800, 
+                                   color='red').add_to(m)
         except:
-            folium.plugins.AntPath(locations=result['tour_coords'], dash_array=[8, 100], delay=800, color='red').add_to(m)
+            folium.plugins.AntPath(locations=result['tour_coords'], dash_array=[8, 100], 
+                                   delay=800, color='red').add_to(m)
             st.warning("Rate limit for HERE service has been reached", icon="⚠️")
     st_data = st_folium(m, width=725)
 
+    ###################################################################################################
+    # MULTISCENARIO SIMULATIONS #######################################################################
+    ###################################################################################################
     if 'nodes_demand_multiscenario' in result.keys():
         st.header('Simulation info:', divider='red')
         d_ms = result['nodes_demand_multiscenario']
@@ -182,7 +243,8 @@ if "result" in st.session_state:
         demands = []
         for i in range(result['num_nodes']):
             mu, sigma = params_simulation[i]
-            demand, bin = np.histogram(demandDataSelected[demandDataSelected['codnode'] == nodeDataSelected['codnode'].values[i]]['Pallets'].tolist())
+            demand, bin = np.histogram(demandDataSelected[demandDataSelected['codnode'] == 
+                                                          nodeDataSelected['codnode'].values[i]]['Pallets'].tolist())
             demands.append(demand)
             mus.append(mu)
             sigmas.append(sigma)
@@ -192,16 +254,27 @@ if "result" in st.session_state:
         for s in range(result['num_scenarios']):
             combined_data.append(('Demand scenario {}'.format(s+1),) + tuple(d_ms[s]))
         
-        df_ms = pd.DataFrame(combined_data, columns=['Variable'] + ['Node {}'.format(nodeDataSelected['codnode'].values[i]) for i in range(result['num_nodes'])])
+        df_ms = pd.DataFrame(combined_data, columns=['Variable'] + ['Node {}'.format(
+            nodeDataSelected['codnode'].values[i]) for i in range(result['num_nodes'])])
         # Traspose DataFrame
         df_ms = df_ms.set_index('Variable').T
-        df_ms.index = ['Node {}'.format(nodeDataSelected['codnode'].values[i]) for i in range(result['num_nodes'])]
-        st.dataframe(df_ms, use_container_width=True, column_config={"distribution": st.column_config.BarChartColumn("Demand Distribution", y_min=0, y_max=80),},)
+        df_ms.index = ['Node {}'.format(nodeDataSelected['codnode'].values[i]) 
+                       for i in range(result['num_nodes'])]
+        st.dataframe(df_ms, use_container_width=True, 
+                     column_config={"distribution": 
+                                    st.column_config.BarChartColumn("Demand Distribution", y_min=0, y_max=80),},)
+        
+        
+    #################################################################################################
+    # STADISTICS                #####################################################################
+    #################################################################################################
 
     st.header('Stadistics:', divider='red')
 
-    st.markdown(f'🚚 Truck with a capacity of **{result['total_capacity']}** doing a distance of **{round(result['total_distance'],2)} Km**')
-    st.markdown(f'**{result['num_visited']}** points have been visited using **{round(result['capacity_used'], 2)}** capacity unit')
+    st.markdown(f'🚚 Truck with a capacity of **{result['total_capacity']}** doing a distance of **{round(
+        result['total_distance'],2)} Km**')
+    st.markdown(f'**{result['num_visited']}** points have been visited using **{round(
+        result['capacity_used'], 2)}** capacity unit')
 
     # Compute percentages
     percentage_clients_delivered = (result['num_visited'] / result['num_nodes']) * 100
@@ -239,9 +312,9 @@ if "result" in st.session_state:
 
     st.altair_chart(chart, use_container_width=True)
 
-    # Show Compute percentages
-    # st.markdown('**Percentage delivered:** ' + str(round(percentage_delivered,2)) + '%')
-    # st.markdown('**Percentage of truck filling:** ' + str(round(percentage_truck_filling,2)) + '%')
+    ################################################################################################
+    # DATA USED                #####################################################################
+    ################################################################################################
 
     st.header('Data:', divider='red')
     # Show the data 
@@ -252,7 +325,7 @@ if "result" in st.session_state:
     demandDataSelected_df = dataframe_explorer(demandDataSelected, case=False)
     st.dataframe(demandDataSelected_df, use_container_width=True, hide_index = True)
 
-####################################################################################
+################################################################################################
 # os.system('streamlit run c:/Users/ctff0/OneDrive/Escritorio/TFM/StocasticOpt/main.py')
 
 
